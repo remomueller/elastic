ffdd766a3dc4cdc6514566e6bde132cd0921476b
Remo Mueller <remosm@gmail.com>
2012-01-17 13:52:40 -0500

    Text fields can't be given a default value in MySQL 5.5.20

--- db/migrate/20110725133950_create_segments.rb
+++ db/migrate/20110725133950_create_segments.rb
@@ -3,10 +3,10 @@ class CreateSegments < ActiveRecord::Migration
     create_table :segments do |t|
       t.integer :user_id
       t.string  :name,                    :null => false, :default => ''
-      t.text    :files,                   :null => false, :default => ''
-      t.text    :trackers,                :null => false, :default => ''
+      t.text    :files,                   :null => false
+      t.text    :trackers,                :null => false
       t.string  :torrent_file,            :null => false, :default => ''
-      t.text    :comments,                :null => false, :default => ''
+      t.text    :comments,                :null => false
       t.integer :torrent_creation_time,   :null => false, :default => 0
 
       t.timestamps
