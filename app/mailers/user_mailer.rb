a202fb0fb53f63ee51461a36fe8a1a59b572fb7a
Remo Mueller <remosm@gmail.com>
2012-08-13 11:32:11 -0400

    Email Changes, default app name in from field
    * Default application name is now added to the from: field for emails
    
    * Email subjects no longer include the application name

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,26 +1,25 @@
 class UserMailer < ActionMailer::Base
-  default :from => ActionMailer::Base.smtp_settings[:user_name]
+  default from: "#{DEFAULT_APP_NAME} <#{ActionMailer::Base.smtp_settings[:user_name]}>"
 
   def notify_system_admin(system_admin, user)
     setup_email
     @system_admin = system_admin
     @user = user
-    mail(:to => system_admin.email,
-         :subject => @subject + "#{user.name} Signed Up",
-         :reply_to => user.email)
+    mail(to: system_admin.email,
+         subject: "#{user.name} Signed Up",
+         reply_to: user.email)
   end
 
   def status_activated(user)
     setup_email
     @user = user
-    mail(:to => user.email,
-         :subject => @subject + "#{user.name}'s Account Activated")
+    mail(to: user.email,
+         subject: "#{user.name}'s Account Activated")
   end
 
   protected
 
   def setup_email
-    @subject = "[#{DEFAULT_APP_NAME}#{'-development' if Rails.env == 'development'}] "
     @footer_html = "Change email settings here: <a href=\"#{SITE_URL}/settings\">#{SITE_URL}/settings</a>.".html_safe
     @footer_txt = "Change email settings here: #{SITE_URL}/settings."
   end
